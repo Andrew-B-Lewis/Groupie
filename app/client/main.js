72a95eabfefbe178a28f7a80e2c777049c5cadca
Brother911 <43596436+Brother911@users.noreply.github.com>
2018-12-01 23:49:38 -0800

    javascript compiler working

--- app/client/main.js
+++ app/client/main.js
@@ -4,5 +4,9 @@ import './main.html';
 
 Template.hello.events({
 
-
+  'submit .file': function (event) {
+    event.preventDefault();
+    const target = event.target;
+    eval(target.code.value);
+  }
 });
